9beaae92664992a53eac740342fd21ccc94eb8ce
Roxana <ms.roxanarahimi@gmail.com>
2022-07-07 12:34:39 +0430

    cart

--- src/views/Foods.vue
+++ src/views/Foods.vue
@@ -1,6 +1,7 @@
 <template>
   <!--      top nav -->
-  <div class="w-100 pt-4 pb-4  d-flex justify-content-center" style="position: fixed; z-index: 100; background-color: white !important ;">
+  <div class="w-100 pt-4 pb-4  d-flex justify-content-center"
+       style="position: fixed; z-index: 100; background-color: white !important ;">
     <div class="btn-group  " style="">
       <a class="btn btn-lg btn-danger p-3" style="border-radius:  0 80px  80px 0"><i
           class="bi bi-caret-right-fill"></i></a>
@@ -21,66 +22,72 @@
   <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
     <div class="offcanvas-header">
       <button type="button" class=" btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      <h5 class="offcanvas-title" id="offcanvasExampleLabel">سبد خرید</h5>
+      <h5 class="offcanvas-title" id="offcanvasExampleLabel"> </h5>
     </div>
     <div class="offcanvas-body">
 
       <!--        factor-->
       <div id="factor1" class="d-xl-none  ">
-        <div class="card" style="min-height :167px;  margin-top: 85px;">
+        <div class="card" style="min-height :167px;  margin-top: 0px;">
           <div class="card-body px-2 py-3">
-            <table class="table  m-0" style="border-radius: 20px">
-              <!--                  <thead class="">-->
-              <!--                  <th>عنوان</th>-->
-              <!--                  <th style="width: 80px">تعداد</th>-->
-              <!--                  </thead>-->
-              <tbody class=" ">
-              <tr>
-                <td>
-                  <p class="fw-bold mb-1">پیتزا ایتالیایی بزرگ</p>
-                  <p class="text-muted  mb-1">198,000</p>
-                </td>
-                <td style="width: 80px"><i class="bi bi-plus-circle-fill me-2 text-danger"></i>1<i
-                    class="bi bi-dash-circle-fill ms-2 text-danger"></i></td>
-              </tr>
-              <tr>
-                <td>
-                  <p class="fw-bold mb-1">برگر ذغالی</p>
-                  <p class="text-muted mb-1">220,000</p>
-                </td>
-                <td><i class="bi bi-plus-circle-fill me-2 text-danger"></i>2<i
-                    class="bi bi-dash-circle-fill ms-2 text-danger"></i></td>
-              </tr>
-              <tr>
-                <td>
-                  <p class="fw-bold mb-1">پپسی</p>
-                  <p class="text-muted mb-1">42,000</p>
-                </td>
-                <td><i class="bi bi-plus-circle-fill me-2 text-danger"></i>3<i
-                    class="bi bi-dash-circle-fill ms-2 text-danger"></i></td>
-              </tr>
-              <tr class="border-none">
-                <th class="border-none">
-                  <p class="fw-bold  mb-1">تخفیف</p>
-                  <p class="fw-bold mb-1">جمع کل</p>
-
-                </th>
-                <td class="border-none">
-                  <p class="fw-bold mb-1">0</p>
-                  <p class="fw-bold mb-1">460,000</p>
-
-                </td>
-
-              </tr>
-              </tbody>
-            </table>
             <div>
-              <button class="btn btn-block  w-100 btn-danger">ثبت نهایی</button>
+              <span class="float-start h4 ">سبد خرید</span>
+              <span class="float-end h4 " style="cursor: pointer"><i class="bi bi-trash-fill"></i></span>
+            </div>
+            <hr class="mt-5">
+
+            <div style="height: 200px; overflow-y: scroll">
+              <table class="table m-0" style="border-radius: 20px">
+                <!--                  <thead class="">-->
+                <!--                  <th>عنوان</th>-->
+                <!--                  <th style="width: 80px">تعداد</th>-->
+                <!--                  </thead>-->
+                <tbody class=" ">
+                <tr>
+                  <td>
+                    <p class="fw-bold mb-1">پیتزا ایتالیایی بزرگ</p>
+                    <p class="text-muted  mb-1">198,000</p>
+                  </td>
+                  <td style="width: 80px"><i class="bi bi-plus-circle-fill me-2 text-danger"></i>1<i
+                      class="bi bi-dash-circle-fill ms-2 text-danger"></i></td>
+                </tr>
+                <tr>
+                  <td>
+                    <p class="fw-bold mb-1">برگر ذغالی</p>
+                    <p class="text-muted mb-1">220,000</p>
+                  </td>
+                  <td><i class="bi bi-plus-circle-fill me-2 text-danger"></i>2<i
+                      class="bi bi-dash-circle-fill ms-2 text-danger"></i></td>
+                </tr>
+                <tr>
+                  <td>
+                    <p class="fw-bold mb-1">پپسی</p>
+                    <p class="text-muted mb-1">42,000</p>
+                  </td>
+                  <td><i class="bi bi-plus-circle-fill me-2 text-danger"></i>3<i
+                      class="bi bi-dash-circle-fill ms-2 text-danger"></i></td>
+                </tr>
+                </tbody>
+              </table>
 
             </div>
+            <!--             <div style="position: absolute; bottom: 10px; right: 5px; width: 97%">-->
+
+            <div class="row">
+              <p class="fw-bold  mb-1 col-6">تخفیف</p>
+              <p class="fw-bold mb-1 col-6">0</p>
+
+              <p class="fw-bold  col-6">جمع کل</p>
+              <p class="fw-bold  col-6">460,000</p>
+            </div>
+            <a class="btn btn-block w-100 btn-danger">ثبت نهایی</a>
+            <!--                </div>-->
 
           </div>
         </div>
+
+        <img style="position: fixed;  width: 350px; bottom: 0px" src="../assets/chef1.png" alt="">
+
       </div>
       <!--        factor-->
 
@@ -118,7 +125,7 @@
 
           <!--          cards-->
           <div class="col-12 col-xl-8 px-md-3">
-            <div id="pizza"    style="" class="row  ">
+            <div id="pizza" style="" class="row  ">
               <div class="col-12 col-md-6 col-lg-4  col-xxl-4 px-3 pe-md-0 ps-md-2 mb-2">
                 <food-card index="1"/>
               </div>
@@ -132,7 +139,7 @@
                 <food-card index="11"/>
               </div>
             </div>
-            <div id="burger"   style="" class="row  ">
+            <div id="burger" style="" class="row  ">
 
               <div class="col-12 col-md-6 col-lg-4  col-xxl-4 px-3 pe-md-0 ps-md-2 mb-2">
                 <food-card index="11"/>
@@ -165,7 +172,7 @@
                 <food-card index="14"/>
               </div>
             </div>
-            <div id="pasta"    style="" class="row  ">
+            <div id="pasta" style="" class="row  ">
 
               <div class="col-12 col-md-6 col-lg-4  col-xxl-4 px-3 pe-md-0 ps-md-2 mb-2">
                 <food-card index="15"/>
@@ -174,14 +181,14 @@
                 <food-card index="16"/>
               </div>
             </div>
-            <div id="side"     style="" class="row  ">
+            <div id="side" style="" class="row  ">
 
 
               <div class="col-12 col-md-6 col-lg-4  col-xxl-4 px-3 pe-md-0 ps-md-2 mb-2">
                 <food-card index="17"/>
               </div>
             </div>
-            <div id="salad"    style="" class="row  ">
+            <div id="salad" style="" class="row  ">
 
               <div class="col-12 col-md-6 col-lg-4  col-xxl-4 px-3 pe-md-0 ps-md-2 mb-2">
                 <food-card index="18"/>
@@ -190,7 +197,7 @@
                 <food-card index="19"/>
               </div>
             </div>
-            <div id="drink"    style="" class="row  ">
+            <div id="drink" style="" class="row  ">
 
               <div class="col-12 col-md-6 col-lg-4  col-xxl-4 px-3 pe-md-0 ps-md-2 mb-2">
                 <food-card index="20"/>
@@ -208,7 +215,7 @@
                 <food-card index="24"/>
               </div>
             </div>
-            <div id="other"    style="" class="row  ">
+            <div id="other" style="" class="row  ">
 
               <div class="col-12 col-md-6 col-lg-4  col-xxl-4 px-3 pe-md-0 ps-md-2 mb-2">
                 <food-card index="25"/>
@@ -233,7 +240,7 @@
                 </div>
                 <hr class="mt-5">
 
-                <div style="height: 300px; overflow-y: scroll">
+                <div style="height: 200px; overflow-y: scroll">
                   <table class="table m-0" style="border-radius: 20px">
                     <!--                  <thead class="">-->
                     <!--                  <th>عنوان</th>-->
@@ -264,30 +271,26 @@
                       <td><i class="bi bi-plus-circle-fill me-2 text-danger"></i>3<i
                           class="bi bi-dash-circle-fill ms-2 text-danger"></i></td>
                     </tr>
-                    <tr class="border-none">
-                      <th class="border-none">
-                        <p class="fw-bold  mb-1">تخفیف</p>
-                        <p class="fw-bold mb-1">جمع کل</p>
-
-                      </th>
-                      <td class="border-none">
-                        <p class="fw-bold mb-1">0</p>
-                        <p class="fw-bold mb-1">460,000</p>
-
-                      </td>
-
-                    </tr>
                     </tbody>
                   </table>
 
                 </div>
                 <!--             <div style="position: absolute; bottom: 10px; right: 5px; width: 97%">-->
+
+                <div class="row">
+                  <p class="fw-bold  mb-1 col-6">تخفیف</p>
+                  <p class="fw-bold mb-1 col-6">0</p>
+
+                  <p class="fw-bold  col-6">جمع کل</p>
+                  <p class="fw-bold  col-6">460,000</p>
+                </div>
                 <a class="btn btn-block w-100 btn-danger">ثبت نهایی</a>
                 <!--                </div>-->
 
               </div>
             </div>
-            <img style="position: fixed; left: 350px; width: 350px; bottom: 0px" src="../assets/chef1.png" alt="">
+
+            <img style="position: fixed;  width: 350px; bottom: 0px" src="../assets/chef1.png" alt="">
 
           </div>
           <!--        factor-->
@@ -339,4 +342,9 @@ table {
 /*    left: 0px;*/
 /*  }*/
 /*}*/
+
+/*.table>:not(caption)>*>* {*/
+/*  border-bottom-width: 0px;*/
+/*}*/
+
 </style>
